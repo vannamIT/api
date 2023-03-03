8c892d59bf290b4fad2ad3bcb6dccaad5489617b
vannamIT <117706180+vannamIT@users.noreply.github.com>
2023-03-03 22:51:51 +0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,12 +1,3 @@
 function myFunction() {
-      try {
-           String url = "https://5gsieutoc.com/api/v1/client/subscribe?token=989c672fee7af3a765ecbd2315e1c973&flag=v2rayn";
-           Document doc = Jsoup.connect(url).get();
-           Elements element = doc.select("p");
-
-           for (Element t : element) {
-             System.out.println(t.text());
-           }
-
-         }
+    document.getElementById("demo").innerHTML="LÊN Luôn";
 }
